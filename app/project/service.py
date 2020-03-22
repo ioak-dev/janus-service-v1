5e29f8f7c35b8d186d486e0186b9db7821ac3bc8
Arun Kumar Selvaraj <dev@ioak.org>
2020-03-22 15:09:55 +0530

    Implementation to move tasks between stages; Sequence implementation and ordering of tasks

--- app/project/service.py
+++ app/project/service.py
@@ -1,15 +1,21 @@
 import os, datetime, time
 from library.db_connection_factory import get_collection
 import library.db_utils as db_utils
+import app.sequence.service as sequence_service
 
 domain = 'Project'
 
 def find(request, space):
-    data = db_utils.find(space, domain, {})
-    return (200, {'data': data})
+    return (200, {'data': find_all_projects(space)})
 
 def update(request, space, data):
+    new_record = False
+    if '_id' not in data:
+        new_record = True
     updated_record = db_utils.upsert(space, domain, data, request.user_id)
+    if new_record:
+        sequence_service.create_sequence(space, 'taskOrder', updated_record['_id'], 1)
+        sequence_service.create_sequence(space['name'], 'taskId', project['_id'], 1)
     return (200, {'data': updated_record})
 
 def delete(request, space, id):
@@ -19,3 +25,6 @@ def delete(request, space, id):
 def find_by_id(request, space, id):
     data = db_utils.find(space, domain, {'_id': id})
     return (200, {'data': data})
+
+def find_all_projects(space):
+    return db_utils.find(space, domain, {})
--- app/task/service.py
+++ app/task/service.py
@@ -1,15 +1,20 @@
-import os, datetime, time
+import os, datetime, time, pymongo
 from library.db_connection_factory import get_collection
 import library.db_utils as db_utils
+from app.sequence.service import nextval, reset_sequence
 
 domain = 'Task'
 
 def find(request, space, project_id):
-    data = db_utils.find(space, domain, {'projectId': project_id})
+    data = db_utils.find(space, domain, {'projectId': project_id}, [('order', pymongo.DESCENDING)])
     return (200, {'data': data})
 
 def update(request, space, project_id, data):
+    project = db_utils.find(space, 'Project', {'_id': project_id})[0]
     data['projectId'] = project_id
+    if '_id' not in data:
+        data['taskId'] = project['name'][:4].upper() + '-' + str(nextval(space, 'taskId', project_id))
+        data['order'] = nextval(space, 'taskOrder', project_id)
     updated_record = db_utils.upsert(space, domain, data, request.user_id)
     return (200, {'data': updated_record})
 
@@ -24,8 +29,23 @@ def find_by_id(request, space, project_id, id):
 def move_task(request, space, project_id, data):
     moveTask = db_utils.find(space, domain, {'_id': data['moveTaskId']})[0]
     afterTask = db_utils.find(space, domain, {'_id': data['afterTaskId']})[0]
-    # inc = afterTask['order'] + 1
-    # if inc not ending in 0, moveTask['order'] = inc
+    inc = afterTask['order'] + 1
+    if moveTask['order'] != inc:
+        print(moveTask['order'], afterTask['order'], inc)
+        recompute_order(space, project_id, afterTask['order'])
+        moveTask['order'] = inc
+    else:
+        sub = afterTask['order']
+        afterTask['order'] = moveTask['order']
+        moveTask['order'] = sub
+        updated_record = db_utils.upsert(space, domain, afterTask, request.user_id)
+
     moveTask['stageId'] = afterTask['stageId']
     updated_record = db_utils.upsert(space, domain, moveTask, request.user_id)
     return (200, {'data': updated_record})
+
+def recompute_order(space, project_id, order):
+    tasks = db_utils.find(space, domain, {'$and': [{'projectId': project_id}, {'order': {'$gt': order}}]}, [('order', pymongo.ASCENDING)])
+    for task in tasks:
+        task['order'] = nextval(space, 'taskOrder', project_id)
+        db_utils.upsert(space, domain, task)
